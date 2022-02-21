a088e89e03065bf0bdc1f105aaee849aaa0f955e
Jared Silver <jaredsilver@Jareds-MacBook-Pro.local>
2022-02-21 15:48:55 -0800

    added localstorage for registered user

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,14 +1,29 @@
 import React, { useState, useReducer, useContext } from 'react'
+import axios from 'axios'
 
 import reducer from './reducers'
 
-import { CLEAR_ALERT, DISPLAY_ALERT } from './actions'
+import {
+  CLEAR_ALERT,
+  DISPLAY_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+} from './actions'
+
+const token = localStorage.getItem('token')
+const user = localStorage.getItem('user')
+const userLocation = localStorage.getItem('location')
 
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: '',
   alertType: '',
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || '',
+  jobLocation: userLocation || '',
 }
 
 const AppContext = React.createContext()
@@ -29,8 +44,39 @@ const AppProvider = ({ children }) => {
     }, 3000)
   }
 
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem('token', token)
+    localStorage.setItem('location', location)
+  }
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    localStorage.removeItem('location')
+  }
+
+  const registerUser = async (currentUser) => {
+    dispatch({ type: REGISTER_USER_BEGIN })
+    try {
+      const response = await axios.post('/api/auth/register', currentUser)
+      const { user, token, location } = response.data
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: { user, token, location },
+      })
+      addUserToLocalStorage({ user, token, location })
+    } catch (error) {
+      dispatch({
+        type: REGISTER_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      })
+    }
+    clearAlert()
+  }
+
   return (
-    <AppContext.Provider value={{ ...state, displayAlert }}>
+    <AppContext.Provider value={{ ...state, displayAlert, registerUser }}>
       {children}
     </AppContext.Provider>
   )
